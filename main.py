ef8e993752920cb8da6fbc26e5e0b959b6be2406
Fiqi Gading <98000756+saikanov@users.noreply.github.com>
2023-05-28 15:15:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ import numpy as np
 import scipy 
 import matplotlib.pyplot as plt 
 
+#set config
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden; }
+        footer {visibility: hidden;}
+        </style>
+        """
+
+st.markdown(hide_menu_style, unsafe_allow_html = True)
 
 #fungsi c terukur
 def c_terukur(abs,slope,intercept):
